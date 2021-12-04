6e4d201a667b3c22f9d58b9cb45d372069b81fdc
kototo522 <huyejiben@gmail.com>
2021-12-04 09:35:07 +0900

    add: ListViewのセット

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -10,5 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAdd: Button = findViewById(R.id.btnAdd)
+        val lv: ListView = findViewById(R.id.lv)
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            mutableListOf()
+        )
+        lv.adapter = adapter
     }
 }
