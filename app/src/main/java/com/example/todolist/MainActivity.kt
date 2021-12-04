3163d94ea460e3caaf35db39a9f7eaf89d23c375
kototo522 <huyejiben@gmail.com>
2021-12-04 09:33:15 +0900

    add: スタイルの追加

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.todolist
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
